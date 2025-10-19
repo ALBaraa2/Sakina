e908fee1a44eab7fe21a382ab43e3666f24d06a5
ALBaraa <albaraawork3@gmail.com>
2025-10-19 14:48:04 +0300

    testing auth:sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function () {
     return response()->json(['message' => 'API works!']);
-});
+})->middleware('auth:sanctum');
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',    [AuthController::class, 'login']);
 Route::post('/logout',   [AuthController::class, 'logout'])->middleware('auth:sanctum');
